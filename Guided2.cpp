b3613c3bd3a0690f8374cbdc033f901256625dc0
Muhammad Fachri <fachriauravyano@gmail.com>
2025-09-29 18:44:47 +0700

    Delete Guided2.cpp

--- Guided2.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-char ch;
-cout << "Masukkan sebuah karakter: ";
-ch = getchar(); // Menggunakan getchar() untuk membaca satu karakter
-cout << "Karakter yang Anda masukkan adalah: " << ch << endl;
-return 0;
-}
