49f16d1764917aeed3c22660237e280a076e1736
Muhammad Fachri <fachriauravyano@gmail.com>
2025-09-29 18:42:51 +0700

    Add files via upload

---
+++ Guided2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+char ch;
+cout << "Masukkan sebuah karakter: ";
+ch = getchar(); // Menggunakan getchar() untuk membaca satu karakter
+cout << "Karakter yang Anda masukkan adalah: " << ch << endl;
+return 0;
+}
---
+++ Unguided1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float bil1, bil2;
+
+    cout << "bilangan pertama: ";
+    cin >> bil1;
+    cout << "bilangan kedua: ";
+    cin >> bil2;
+
+    cout << "hasil penjumlahan: " << bil1 + bil2 << endl;
+    cout << "hasil pengurangan: " << bil1 - bil2 << endl;
+    cout << "hasil perkalian: " << bil1 * bil2 << endl;
+    cout << "hasil pembagian: " << bil1 / bil2 << endl;
+    return 0;
+}
